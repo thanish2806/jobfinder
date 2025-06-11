914e6d586bd916f132635500989b5a181aa17622
Thanish <thanish059@gmail.com>
2025-06-11 19:53:42 +0530

    Initial commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/skillnest/', // ✅ Required for GitHub Pages
   server: {
     proxy: {
-      '/api': 'http://localhost:5000', // 🔁 Proxy API calls to backend (only for dev)
+      '/api': 'http://localhost:5000', // 🔁 Proxy API calls to backend
     },
+    historyApiFallback: true, // ✅ Handles SPA routing
   },
+  base: '/',
 });
