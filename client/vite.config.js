ea7ad06e740b465fb33d34e917669638b9a98122
Thanish <101385978+thanish2806@users.noreply.github.com>
2025-06-12 18:29:49 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/skillify/',
+  base: '/',
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
