d0c8a1f6099a65246f652cd39622ef3ccbbcaf13
Thanish <101385978+thanish2806@users.noreply.github.com>
2025-06-12 18:00:21 +0530

    Update Chatbot.jsx

--- client/src/Chatbot.jsx
+++ client/src/Chatbot.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 import './Chatbot.css';
 
-const socket = io('http://localhost:5000');
+const socket = io('https://jobfinder-backend-m3fz.onrender.com', {
+  transports: ['websocket'],
+});
 
 function Chat() {
   const [input, setInput] = useState('');
