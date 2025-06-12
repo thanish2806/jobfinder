91a20dbd9c9e56696504bf8d630e8fdc331d10e6
Thanish <101385978+thanish2806@users.noreply.github.com>
2025-06-12 17:57:19 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,7 +22,13 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5174'], // ✅ added 5174
+    origin: [
+  'http://localhost:5173',
+  'http://localhost:3000',
+  'http://localhost:5174',
+  'https://thanish2806.github.io'
+],
+
     methods: ['GET', 'POST'],
     credentials: true,
   },
@@ -30,7 +36,13 @@ const io = new Server(server, {
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5174'], // ✅ added 5174
+  origin: [
+  'http://localhost:5173',
+  'http://localhost:3000',
+  'http://localhost:5174',
+  'https://thanish2806.github.io'
+],
+
   methods: ['GET', 'POST'],
   credentials: true,
 }));
