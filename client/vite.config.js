c25887b9f80972910fe53d8400564e4bcfe69d8d
Thanish <thanish059@gmail.com>
2025-06-15 09:28:38 +0530

    Resolved merge conflicts

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-=======
   base: '/',
->>>>>>> 4bea35638929896f45d54f56a204d706ff6b96fc
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
