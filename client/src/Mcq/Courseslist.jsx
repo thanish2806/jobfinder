c2ccb011e6a00e2dc61ed73dfa66e61983c21220
Thanish <thanish059@gmail.com>
2025-06-26 19:40:40 +0530

    in course list page start learning changed to take quiz

--- client/src/Mcq/Courseslist.jsx
+++ client/src/Mcq/Courseslist.jsx
@@ -98,7 +98,7 @@ const CoursesList = () => {
                     }
                   }}
                 >
-                  Start Learning
+                  Take Test
                 </button>
               </div>
             ))
